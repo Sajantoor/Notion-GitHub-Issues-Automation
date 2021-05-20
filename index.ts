24ccb81db75807a9d654ff038af4cdcff41b03d4
Sajan Toor <sajantoor98@gmail.com>
2021-05-19 18:39:31 -0700

    :sparkles: Wrote building blocks, installed notion package

--- index.ts
+++ index.ts
@@ -1,22 +1,65 @@
 import dotenv = require('dotenv');
 import fetch from 'node-fetch';
-
-/* -----------------
-   Startup
-   ----------------
-*/
+// import Client from '@notionhq/client';
+const { Client } = require('@notionhq/client');
 
 // get info from .env file
 dotenv.config();
 
-let repoName = process.env.repo;
+const USERNAME = process.env.user;
+const REPO = process.env.repo;
 
-// fetch from github api for new issues 
-// access notion api to post to database containing github repo details
+const GITHUB_API = `https://api.github.com/repos/${USERNAME}/${REPO}/issues`
+const NOTION_API = new Client({ auth: process.env.NOTION_API_KEY });
+const DATABASE_ID = process.env.NOTION_DATABASE;
 
 /**
- * Fetch from the GitHub API to get new issues related to this repo 
+ * Fetch from the GitHub API to get new issues related to our working repo 
+ * @return Response a promise from the  GitHub API
  */
-function fetchGithub(): void {
-
+async function fetchGitHub() {
+    let response = await fetch(GITHUB_API);
+    let data = await response.json();
+    return data;
 }
+
+
+/**
+ * Fetch from the Notion API and get information about our database
+* @return returns a promise from the Notion API 
+*/
+async function fetchNotion() {
+    const response = await NOTION_API.databases.retrieve({
+      database_id: DATABASE_ID,
+    });
+
+    return response;
+}
+
+/**
+ * Post to the Notion API 
+ */
+async function postNotion() {
+    const response = await NOTION_API.pages.create({
+        parent: {
+            database_id: DATABASE_ID,
+        },
+        properties: {
+            'Name': {
+              type: 'title',
+              title: [
+                {
+                  type: 'text',
+                  text: {
+                    content: 'Hello, World!',
+                  },
+                },
+              ],
+            },
+          },
+    });
+
+    console.log(response);
+}
+
+// postNotion();
